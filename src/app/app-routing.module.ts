aed9cea0c5e9c887d354e1eab7db65765a778c9c
Yogesh <yogeshbietjhs@gmail.com>
2022-10-16 13:54:53 +0530

    Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-
+  {path:'login', component:LoginComponent},
   {path:'dashboard', component:DashboardComponent}
 ];
 
