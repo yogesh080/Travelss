b52b5ccaf82676f16759c183db77e4f0fd18e15a
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-10-19 15:51:35 +0530

    [Abhishek]footer added needs modification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,22 @@ import { DisplaystateComponent } from './components/displaystate/displaystate.co
 import { BodyComponent } from './components/body/body.component';
 import { SliderComponent } from './components/slider/slider.component';
 
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -54,6 +70,7 @@ import { SliderComponent } from './components/slider/slider.component';
     DisplaystateComponent,
     BodyComponent,
     SliderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -74,7 +91,22 @@ import { SliderComponent } from './components/slider/slider.component';
     AngularFireModule.initializeApp(environment.firebase),
     MatProgressBarModule,
     NgbModule,
-    NgImageSliderModule
+    NgImageSliderModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule
     
   ],
   providers: [],
